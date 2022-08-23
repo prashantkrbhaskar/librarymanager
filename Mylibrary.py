c53e0bd87cf8315941d5ac2434c57ee81e075e82
prashantkrbhaskar <87667124+prashantkrbhaskar@users.noreply.github.com>
2022-08-23 19:53:20 +0530

    Update Mylibrary.py

--- Mylibrary.py
+++ Mylibrary.py
@@ -1,3 +1,5 @@
+# Library manager
+# code by prashant kumar bhaskar
 def getdate():
     import datetime
     return datetime.datetime.now()
@@ -10,41 +12,57 @@ class Library:
         for book in self.books:
             print(" ♦-- " + book)
         print("\n")
-
     def borrowBook(self, name, bookname):
         if bookname not in self.books:
             print(
                 f"{bookname} BOOK IS NOT AVAILABLE EITHER TAKEN BY SOMEONE ELSE, WAIT UNTIL HE RETURNED.\n")
         else:
             track.append({name: bookname})
-            print("BOOK ISSUED : THANK YOU KEEP IT WITH CARE AND RETURN ON TIME.\n")
             self.books.remove(bookname)
-
+            with open("booklogs.txt", "a") as b:
+                b.write(str([str(getdate())]) + "-" + bookname + "," + "borrowed by-" + name + "\n")
+                print("Thank you")
+            print("BOOK ISSUED : THANK YOU KEEP IT WITH CARE AND RETURN ON TIME.\n")
+            with open('books.txt','r') as file:
+                filedata=file.read()
+                filedata=filedata.replace(bookname,"")
+                with open('books.txt','w') as file:
+                    file.write(filedata)
     def returnBook(self, bookname):
         print("BOOK RETURNED : THANK YOU! \n")
         self.books.append(bookname)
+        with open("book.txt", "a") as bk:
+            bk.write(bookname+"\n")
 
     def donateBook(self, bookname):
         print("BOOK DONATED : THANK YOU VERY MUCH, HAVE A GREAT DAY AHEAD.\n")
         self.books.append(bookname)
-
+        with open("bookdonationlogs.txt", "a") as d:
+            print("please enter your name")
+            v=input()
+            d.write(str([str(getdate())]) + "-" + bookname +","+ "donated by-"+v+"\n")
+            print("Thank you for donating")
+        with open("book.txt", "a") as bk:
+            bk.write(bookname+"\n")
 
 class Student():
     def requestBook(self):
-        print("So, you want to borrow book!")
         self.book = input("Enter name of the book you want to borrow: ")
         return self.book
 
+
     def returnBook(self):
-        print("So, you want to return book!")
         name = input("Enter your name: ")
         self.book = input("Enter name of the book you want to return: ")
         if {name: self.book} in track:
             track.remove({name: self.book})
+        with open("booklogs.txt", "a") as b:
+            b.write(str([str(getdate())]) + "-" + self.book +","+ "returned by-"+name+"\n")
+            print("Thank you")
         return self.book
 
+
     def donateBook(self):
-        print("Okay! you want to doante book!")
         self.book = input("Enter name of the book you want to donate: ")
         return self.book
 
@@ -57,13 +75,9 @@ if __name__ == "__main__":
     track = []
 
     print("\t\t\t\t\t\t\t♦♦♦♦♦♦♦ WELCOME TO THE MY LIBRARY ♦♦♦♦♦♦♦\n")
-    # print(
-    #     """CHOOSE WHAT YOU WANT TO DO:-\n1. Listing all books\n2. Borrow books\n3. Return books\n4. Donate books\n5. Track books\n6. exit the library\n""")
-
     while (True):
         # print(track)
-        print(
-            """CHOOSE WHAT YOU WANT TO DO:-\n1. Listing all books\n2. Borrow books\n3. Return books\n4. Donate books\n5. Track books\n6. exit the library\n""")
+        print("""CHOOSE WHAT YOU WANT TO DO:-\n1. Listing all books\n2. Borrow books\n3. Return books\n4. Donate books\n5. Track books\n6. exit the library\n""")
 
         try:
             usr_response = int(input("Enter your choice: "))
